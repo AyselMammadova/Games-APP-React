4f79bb6c41b3e8ef14ce60cd307837ddfa0ca8cd
Aysel <a1@1-iMac.local>
2022-06-22 18:57:06 +0400

    api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
-
+import { useEffect } from 'react';
+import { useState } from 'react';
 function App() {
+  const [games,setGames] = useState([])
+  useEffect(()=>{
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '9e29e4600fmshd449dc345ba86b9p128ef9jsn5e119316a215',
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+      }
+    };
+    let url = 'https://free-to-play-games-database.p.rapidapi.com/api/games'
+    
+    fetch(url, options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err)); 
+  },[])
   return (
     <>
       <Router>
@@ -12,6 +29,7 @@ function App() {
         <Routes>
           <Route path='/Games-APP-React' element={<Home />} />
         </Routes>
+        
       </Router>
       
     </>
