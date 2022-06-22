a37a6151bdca9a3018f69d39b9accf01ba5b7068
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2022-06-23 00:19:07 +0400

    api

--- src/App.js
+++ src/App.js
@@ -3,25 +3,8 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
-import { useEffect } from 'react';
-import { useState } from 'react';
+
 function App() {
-  const [games,setGames] = useState([])
-  useEffect(()=>{
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '9e29e4600fmshd449dc345ba86b9p128ef9jsn5e119316a215',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-      }
-    };
-    let url = 'https://free-to-play-games-database.p.rapidapi.com/api/games'
-    
-    fetch(url, options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err)); 
-  },[])
   return (
     <>
       <Router>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import CardItem from './CardItem';
 import image from '../img/1.jpg';
 import './Cards.css'
 
 function Cards() {
+  const [games,setGames] = useState({items: []});
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '9e29e4600fmshd449dc345ba86b9p128ef9jsn5e119316a215',
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+      }
+    };
+
+    let url = 'https://free-to-play-games-database.p.rapidapi.com/api/game';
+    console.log(games);
+    fetch(url + '?id=452', options)
+      .then(response => response.json())
+      .then((json) => {
+        setGames({
+          items: json,
+        })
+    }).catch(err => console.error(err));
+  }, [])
+
+  console.log(setGames);
+
+
+
+
   return (
     <div className="cards">
         <h1>Text</h1>
@@ -12,7 +38,7 @@ function Cards() {
                 <div className="cards__items">
                 <CardItem
                     src={image}
-                    text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                    text={setGames[0]}
                     label='Adventure'
                     path='/'
                     />
