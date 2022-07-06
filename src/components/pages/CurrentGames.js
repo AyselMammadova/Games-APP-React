f58cd1517da52b5a516dd40d66a9c55a3af144f6
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2022-07-06 22:01:38 +0400

    dynamically background images

--- src/components/pages/CurrentGames.js
+++ src/components/pages/CurrentGames.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
+import '../main-section/MainSection.css';
 import AllGames from '../cards/AllGames';
 import CardContextProvider from '../context-api/CardContext';
+import img1 from '../../img/1.jpg';
+import img2 from '../../img/2.jpg';
+import img3 from '../../img/3.jpeg';
 
 function CurrentGames() {
+
+    const slider = [
+        img1,
+        img2,
+        img3
+    ]
+
+    let [slideIndex, setSlideIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const nextSlide = ++ slideIndex % slideIndex.length;
+            setSlideIndex(nextSlide);
+        }, 2000);
+        return () => clearInterval(interval);
+    }, [slideIndex]);
+
+    console.log(img1)
 
     const [searchItem, setSearchItem] = useState('');
 
@@ -16,7 +38,7 @@ function CurrentGames() {
     return (
         <>
             <main className="main-sec-pages">
-                <div className="main-container">
+                <div className="main-container" style={{backgroundImage: "url(" + slider[slideIndex] + ")"}}>
                     <div className="search-group input-group flex-nowrap">
                         <span className="input-group-text" id="search">
                             <i className="fas fa-search" />
