de87b69fecbf786c0933e7db111de184b9a5456b
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2022-07-11 23:57:16 +0400

    some changes

--- src/components/context-api/CardContext.js
+++ src/components/context-api/CardContext.js
@@ -7,12 +7,13 @@ function CardContextProvider (props) {
 
     const [games,setGames] = useState([]);
     useEffect(() => {
+
         const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '9e29e4600fmshd449dc345ba86b9p128ef9jsn5e119316a215',
-            'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-        }
+          method: 'GET',
+          headers: {
+              'X-RapidAPI-Key': '9e29e4600fmshd449dc345ba86b9p128ef9jsn5e119316a215',
+              'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+          }
         };
 
         let url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
@@ -21,16 +22,10 @@ function CardContextProvider (props) {
         .then(response => response.json())
         .then(response => setGames(response))
         .catch(err => console.error(err));
+        
     }, []);
 
-
-    // add fav property
-    // games.forEach(game => {
-    //   game.fav = false;
-    // });
-
-    // console.log(games)
-
+    
 
   return (
     <CardContext.Provider value = {{games}}>
